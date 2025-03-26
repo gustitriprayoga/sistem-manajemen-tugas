dd2949a9a6e13f5a76dde38c0fd8553ddd2e8f74
TukS <91913765+gustitriprayoga@users.noreply.github.com>
2025-03-27 00:39:13 +0700

    change dashboard asset

---
+++ app/Http/Controllers/Backend/Setting/SettingController.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Http\Controllers\Backend\Setting;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+class SettingController extends Controller
+{
+    public function index()
+    {
+        return view('backend.setting.index');
+    }
+
+
+}
